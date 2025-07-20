d900c0ae5f8aafaee3f02e15bb197569371b9dfd
Trần Văn Hải Lâm <hailamtranvan@gmail.com>
2025-07-21 02:06:10 +0700

    Add spring cloud config server and client

---
+++ configservice/src/test/java/vnpt_it/vn/configservice/ConfigserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package vnpt_it.vn.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
